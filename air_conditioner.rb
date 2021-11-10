a2a496bc189063990cea415ed352d3825446fa79
AMI-KATSUMATA <amic2264@gmail.com>
2021-11-10 04:34:38 +0000

    [finish]Algorithm6

--- air_conditioner.rb
+++ air_conditioner.rb
@@ -2,6 +2,7 @@ input_lines = readlines
 
 room = input_lines[0].to_i
 air_conditioner = input_lines[1].to_i
+power = input_lines[2].to_i
 
 temperature_diff = (room - air_conditioner).abs
 
@@ -15,4 +16,16 @@ elsif temperature_diff >= 10 then
     require_time = 60
 end
 
-p require_time
+if power == 1
+    final_time = require_time
+elsif power == 2
+    final_time = require_time - 5
+elsif power == 3
+    final_time = require_time - 10
+else
+    p "風量は1~3にして下さい"
+    exit
+end
+
+p final_time
+
